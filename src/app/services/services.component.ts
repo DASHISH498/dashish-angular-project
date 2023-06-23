a9b902a1305f7cbf67d246ed4d3f7c0717efcd0e
ashish-s-eq <designer.ashish@hotmail.com>
2023-06-23 22:11:17 +0530

    service desciprtion added

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -10,27 +10,27 @@ export class ServicesComponent {
     {
       imgname: 'ui_ux_design',
       boxheading: 'UI/UX Design',
-      paragraph: 'Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.'
+      paragraph: 'Creating visually appealing and user-friendly digital interfaces for seamless and engaging experiences.'
     },
     {
       imgname: 'web_development',
       boxheading: 'Web Development',
-      paragraph: 'Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.'
+      paragraph: 'Building and maintaining websites using programming languages, frameworks, and tools to deliver functional and interactive online experiences.'
     },
     {
       imgname: 'reponsive_design',
       boxheading: 'Mobile Friendly',
-      paragraph: 'Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.'
+      paragraph: 'Creating websites that adapt and display seamlessly on various devices, providing an optimal user experience across different screen sizes.'
     },
     {
       imgname: 'graphic_design',
       boxheading: 'Graphic Design',
-      paragraph: 'Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.'
+      paragraph: 'Visual communication through the use of typography, imagery, and layout to convey messages effectively and captivate audiences.'
     },
     {
       imgname: 'clean_code',
       boxheading: 'Clean Code',
-      paragraph: 'Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.'
+      paragraph: 'Developing software solutions with a focus on writing clear, organized, and maintainable code for efficient and sustainable development.'
     }
   ]
 
